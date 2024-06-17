65e3397e1b7ac1511988d09d051987a81e3b30bb
Borjaja2000 <46570584+Borjaja2000@users.noreply.github.com>
2024-06-17 13:39:56 +0200

    Arreglos finales

--- app.py
+++ app.py
@@ -49,15 +49,66 @@ enable_fare_filter = st.sidebar.checkbox("Habilitar filtro por precio del billet
 filtro_billete = st.sidebar.radio('Filtrar por precio Billete', ('Todos', 'Precio mas caro', 'Precio mas barato'))
 precio_minimo = data['Fare'].min()
 precio_maximo = data['Fare'].max()
+def inflation_rate(precio):
+   inflation = precio * 143.15
+   return round(inflation, 2)
+def pounds_to_euros(precio):
+   euros = precio * 1.18
+   return round(euros, 2)  
 if enable_fare_filter:
    if filtro_billete == "Precio mas caro":
       grafico_principal.write(data[data['Fare'] == precio_maximo])
-      st.write(f"El precio mas caro es {precio_maximo.round(2)} libras")
+      st.write(f"El precio mas caro es £{precio_maximo.round(2)}")
+      st.write(f"Hoy en dia sería £{inflation_rate(precio_maximo)} o {pounds_to_euros(inflation_rate(precio_maximo))}€")
+      calculadora = st.checkbox("Habilitar calculadora de inflación", value=False)
+      if calculadora:
+         number = st.number_input("Introduzca un precio en libras", min_value=0.0, step=0.01, format="%.2f")
+         st.write(f"El precio actual es £{inflation_rate(number)} o {pounds_to_euros(inflation_rate(number))}€")
    elif filtro_billete == "Precio mas barato":
       grafico_principal.write(data[data['Fare'] == precio_minimo])
-      st.write(f"El precio mas barato es {precio_minimo.round(2)} libras")
+      st.write(f"El precio mas barato es £{precio_minimo.round(2)}")
    else:
       grafico_principal.write(data)
+
+# Filtro titulos
+def translate_title(title):
+   translations = {
+      'Mr': 'Señor',
+      'Mrs': 'Señora',
+      'Miss': 'Señorita',
+      'Master': 'Maestro',
+      'Dr': 'Doctor',
+      'Rev': 'Reverendo',
+      'Col': 'Coronel',
+      'Major': 'Mayor',
+      'Mlle': 'Señorita',
+      'Mme': 'Señora',
+      'Sir': 'Señor',
+      'Lady': 'Dama',
+      'Countess': 'Condesa',
+      'Don': 'Don',
+      'Jonkheer': 'Jonkheer'
+   }
+   return translations.get(title, title)
+enable_title_filter = st.sidebar.checkbox("Habilitar filtro por título")
+data['Title'] = data['Name'].str.extract('([A-Za-z]+)\.', expand=False)
+titles = data['Title'].unique()
+filtro_titulo = st.sidebar.selectbox("Título para filtrar", titles)
+if enable_title_filter:
+   if filtro_titulo:
+      filtered_data = data[data['Title'] == filtro_titulo]
+      st.write(f"Hay {filtered_data.shape[0]} pasajeros con el título {filtro_titulo} ({translate_title(filtro_titulo)})")
+      grafico_principal.write(filtered_data)
+   else:
+      grafico_principal.write(data)
+
+
+
+# Gráfico de barras para mostrar el precio medio por billete por cada título
+fig_mean_fare = px.bar(data.groupby('Title')['Fare'].mean().reset_index(), x='Title', y='Fare', title='Precio Medio por Billete por Título')
+fig_mean_fare.update_layout(xaxis_title='Título', yaxis_title='Precio Medio del Billete')
+fig_mean_fare.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5)
+st.plotly_chart(fig_mean_fare)
 
 #Configuracion de las graficas
 st.sidebar.title("Configuración gráficas")
@@ -140,14 +191,14 @@ fig_survived_class.update_layout(xaxis_title='Clase de Pasajeros', yaxis_title='
 fig_survived_class.update_traces(marker_color=['rgb(158,202,225)', 'rgb(255,87,87)'], marker_line_color='rgb(8,48,107)', marker_line_width=1.5, selector=dict(type='bar'))
 st.plotly_chart(fig_survived_class)
 
-# Gráfico de barras para mostrar la distribución de la supervivencia por puerto de embarque
-fig_survived_embarked = px.histogram(data, x='Embarked', color='Survived', barmode='group', title='Distribución de la Supervivencia por Puerto de Embarque')
-fig_survived_embarked.update_layout(xaxis_title='Puerto de Embarque', yaxis_title='Total pasajeros', showlegend=True)
-fig_survived_embarked.update_traces(marker_color=['rgb(158,202,225)', 'rgb(255,87,87)'], marker_line_color='rgb(8,48,107)', marker_line_width=1.5, selector=dict(type='bar'))
-st.plotly_chart(fig_survived_embarked)
-
 # Gráfico de barras para mostrar la distribución de la supervivencia por sexo y clase de pasajeros
 fig_survived_sex_class = px.histogram(data, x='Sex', color='Survived', facet_col='Pclass', barmode='group', title='Distribución de la Supervivencia por Sexo y Clase de Pasajeros')
 fig_survived_sex_class.update_layout(xaxis_title='Sexo', yaxis_title='Total pasajeros', showlegend=True)
 fig_survived_sex_class.update_traces(marker_color=['rgb(158,202,225)', 'rgb(255,87,87)'], marker_line_color='rgb(8,48,107)', marker_line_width=1.5, selector=dict(type='bar'))
 st.plotly_chart(fig_survived_sex_class)
+
+# Gráfico de dispersión para mostrar la relación entre la cabina, el precio y la clase
+fig_cabin = px.scatter(data, x='Cabin', y='Fare', color='Pclass', title='Relación entre Cabina, Precio del Billete y Clase', color_continuous_scale='emrld')
+fig_cabin.update_layout(xaxis_title='Cabina', yaxis_title='Precio del Billete')
+st.plotly_chart(fig_cabin)
+
